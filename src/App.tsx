25ce5e2abad7c8c3fc3ee9438cd47107fdd09b58
tw3wt <taewoo136@gmail.com>
2024-11-10 15:56:02 +0900

    sign-in 구현 및 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 // src/App.tsx
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import SignIn from './app/components/sign-in/sign-in.component';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import SignIn from '../src/app/components/sign-in/sign-in.component';
 
 function App() {
   return (
-    <Router>
-      <div id="app">
-        <Routes>
-          {/* 기본 경로에서 /SignIn으로 리디렉션 */}
-          <Route path="/" element={<Navigate to="/SignIn" replace />} />
-          <Route path="/SignIn" element={<SignIn />} />
-        </Routes>
-      </div>
-    </Router>
+    <div id="app">
+      <Routes>
+        {/* 기본 경로에서 /SignIn으로 리디렉션 */}
+        <Route path="/" element={<Navigate to="/SignIn" replace />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
