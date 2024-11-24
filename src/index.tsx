c293fe849a8a80291cd4d97613cd6bbd8dad0267
tw3wt <taewoo136@gmail.com>
2024-11-24 16:11:25 +0900

    라우트 수정

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <BrowserRouter basename="/reactProject">
+    <App />
+  </BrowserRouter>,
   </React.StrictMode>
 );
