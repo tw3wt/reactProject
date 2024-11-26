af1264ec7595f5adfe2a8c07211887b95f2a309d
tw3wt <130429670+tw3wt@users.noreply.github.com>
2024-11-26 11:05:18 +0900

    홈 화면 라우트

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import SignIn from '../src/app/components/sign-in/sign-in.component';
 import './app/components/sign-in/sign-in.component.css'
 import './app/components/sign-in/sign-in.component.tsx'
+import HomeComponent from './app/components/home/home.component';
+import HomeMain from './app/components/home/main/home-main.component';
 
 function App() {
   return (
     <div id="app">
       <Routes>
         {/* 기본 경로에서 /sign-in으로 리디렉션 */}
-        <Route path="/" element={<Navigate to="/sign-in" replace />} />
         <Route path="/sign-in" element={<SignIn />} />
+
+        <Route path='/' element={<HomeMain/>} />
       </Routes>
     </div>
   );
